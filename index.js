3cf1dfc02d949ff68decda2bb82e65c44faf4476
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2025-09-06 21:58:43 +0530

    Redeploy

--- index.js
+++ index.js
@@ -8,41 +8,40 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
-
-app.get("/",(req,res)=>{
-    res.send("Hello world");
-});
-
-app.post("/generate",async (req,res)=>{
-    const {query, video_id,thread_id} = req.body;
-    
-
-
-console.log("Q1-------");
-const result = await agent.invoke(
-  {
-    messages: [
-      {
-        role: "user",
-        content: query,
-      },
-    ],
-  },
-  {
-    configurable: { thread_id, video_id },
-  }
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS"],         
+  allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
-console.log(result.messages.at(-1).content);
-
-res.send(result.messages.at(-1).content);
-
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen(port,()=>{
+app.post("/generate", async (req, res) => {
+  const { query, video_id, thread_id } = req.body;
+  const result = await agent.invoke(
+    {
+      messages: [
+        {
+          role: "user",
+          content: query,
+        },
+      ],
+    },
+    {
+      configurable: { thread_id, video_id },
+    }
+  );
+
+  console.log(result.messages.at(-1).content);
+
+  res.send(result.messages.at(-1).content);
+});
+
+app.listen(port, () => {
   console.log("Server started!");
 });
